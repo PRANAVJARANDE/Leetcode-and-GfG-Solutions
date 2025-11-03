056ffdffe41bc97d7c19a238a5c40edac63bbdcf
pj17 <pranavjarande@gmail.com>
2025-11-03 12:22:44 +0530

    Update Course Schedule II.cpp

--- medium/Course Schedule II.cpp
+++ medium/Course Schedule II.cpp
@@ -47,3 +47,52 @@ public:
         return toptlogicalsort(n,adj,indeg);
     }
 };
+
+
+// Gfg --------------------------------------------------------------
+class Solution {
+  public:
+    vector<int> findOrder(int n, vector<vector<int>> &prerequisites) 
+    {
+        vector<vector<int>>adj(n);
+        vector<int>deg(n);
+        for(auto x:prerequisites)
+        {
+            deg[x[0]]++;
+            adj[x[1]].push_back(x[0]);
+        }
+        queue<int>q1;
+        vector<int>vis(n,0);
+        vector<int>ans;
+        for(int i=0;i<n;i++)
+        {
+            if(deg[i]==0)
+            {
+                ans.push_back(i);
+                q1.push(i);
+                vis[i]=1;
+            }
+        }
+        while(!q1.empty())
+        {
+            auto in=q1.front();
+            q1.pop();
+            for(auto x:adj[in])
+            {
+                if(vis[x]==0)
+                {
+                    deg[x]--;
+                    if(deg[x]==0)
+                    {
+                        q1.push(x);
+                        vis[x]=1;
+                        ans.push_back(x);
+                    }
+                }
+            }
+        }
+        if(ans.size()==n)return ans;
+        return vector<int>(0);
+        
+    }
+};
