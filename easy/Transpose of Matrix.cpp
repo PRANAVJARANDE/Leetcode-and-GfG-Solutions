e8375a4bf632d44d5fac6df466c78006a1a0a443
pj17 <pranavjarande@gmail.com>
2025-12-12 13:25:02 +0530

    Create Transpose of Matrix.cpp

---
+++ easy/Transpose of Matrix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    vector<vector<int>> transpose(vector<vector<int>>& a) {
+        int n=a.size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                swap(a[i][j],a[j][i]);
+            }
+        }
+        return a;
+        
+    }
+};
