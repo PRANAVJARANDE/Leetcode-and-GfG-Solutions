84fb899997b94aa52d8b86cbabd8c61dc8f112e2
pj17 <pranavjarande@gmail.com>
2026-01-24 14:36:49 +0530

    Create Josephus problem.cpp

---
+++ easy/Josephus problem.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int josephus(int n, int k) 
+    {
+        int i=1,ans=0;
+        while (i<=n) 
+        {
+            ans=(ans+k)%i;
+            i++;
+        }
+        return ans+1;
+    }
+};
