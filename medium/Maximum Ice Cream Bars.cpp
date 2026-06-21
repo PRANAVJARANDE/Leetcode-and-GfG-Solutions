971cbe8f9d1e32d15e5f6526bd6890bcc950b3b2
pj17 <pranavjarande@gmail.com>
2026-06-21 15:06:07 +0530

    Create Maximum Ice Cream Bars.cpp

---
+++ medium/Maximum Ice Cream Bars.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) 
+    {
+        int ans=0;
+        sort(costs.begin(),costs.end());
+        for(auto x:costs)
+        {
+            if(x<=coins)
+            {
+                coins-=x;
+                ans++;
+            }
+            else break;
+        }
+        return ans;
+    }
+};
